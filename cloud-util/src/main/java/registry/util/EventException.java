ceab3b26493fec0f0a0e4e33121988af80d84386
bjyfanzhen <anzhen@jd.com>
2016-05-23 20:09:38 +0800

    时间管理器  压缩工具

---
+++ cloud-util/src/main/java/registry/util/EventException.java
@@ -0,0 +1,11 @@
+package registry.util;
+
+/**
+ * Created by anzhen on 2016/5/23.
+ */
+public class EventException extends RuntimeException {
+
+    public EventException(Exception cause) {
+        super(cause);
+    }
+}
